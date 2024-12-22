0cd8d70271461aa1a2507225805e275b8f70ffa5
Aung Moe Oo <aungmoeoo.dev@gmail.com>
2024-12-23 00:03:16 +0630

    feat: add StatePatternMain

---
+++ DesignPatterns/StatePattern/StatePatternMain.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.StatePattern;
+
+public class StatePatternMain
+{
+	public static void Run()
+	{
+		Car car = new Car();
+
+		car.SetState(new IdleState());
+
+		Console.WriteLine(car.Speed);
+
+		car.SetState(new MoveState());
+
+		Console.WriteLine(car.Speed);
+	}
+}
