a0377f6474813c89eb2e9c47d071448c500728d2
Aung Moe Oo <aungmoeoo.dev@gmail.com>
2024-12-22 23:50:44 +0630

    feat: add State Pattern

---
+++ DesignPatterns/StatePattern/Car.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.StatePattern;
+
+public class Car
+{
+	private State _state { get; set; }
+}
---
+++ DesignPatterns/StatePattern/State.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.StatePattern;
+
+public abstract class State
+{
+	private Car _car { get; set; }
+}
